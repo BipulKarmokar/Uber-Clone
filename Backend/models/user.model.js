e2ed2130e717dbe8f0bbae7eaf12d83a11563da9
Bipul Karmokar <karmokarbipul27@gmail.com>
2025-04-23 17:08:54 +0600

    user register and login done

---
+++ Backend/models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        firstName: {
+            type: String,
+            required: true,
+            trim: true,
+            minLength: [3, 'First name must be at least 3 characters long'],
+        },
+        lastName: {
+            type: String,
+            trim: true,
+            minLength: [3, 'Last name must be at least 3 characters long'],
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: [6, 'Password must be at least 6 characters long'],
+        select: false,
+    },
+    socketId: {
+        type: String
+    }
+})
+
+userSchema.methods.generateAuthToken = async function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET)
+    return token
+}
+
+userSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.statics.hashPassword = async function(password) {
+    return await bcrypt.hash(password, 10)
+}
+
+const userModel = mongoose.model('User', userSchema)
+
+module.exports = userModel
+
